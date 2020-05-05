40b02e7e89a89846244c5bf438eb78c84cbb3edd
unknown <rtemkaxxx@gmail.com>
2020-05-05 10:10:50 +0300

    Add DB Seed data.

--- FriendOrganizer.DataAccess/AppDbContext.cs
+++ FriendOrganizer.DataAccess/AppDbContext.cs
@@ -13,7 +13,15 @@ namespace FriendOrganizer.DataAccess
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            builder.UseSqlite("Filename=FriendsOrganizer.db");
+            builder.UseSqlite("Filename=../FriendsOrganizer.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Friend>().HasData(new Friend { Id = 1, FirstName = "Thomas", LastName = "Huber" },
+                                             new Friend { Id = 2, FirstName = "Andreas", LastName = "Boehler" },
+                                             new Friend { Id = 3, FirstName = "Julia", LastName = "Huber" },
+                                             new Friend { Id = 4, FirstName = "Chrissi", LastName = "Egin" });
         }
 
         public DbSet<Friend> Friends { get; set; }
