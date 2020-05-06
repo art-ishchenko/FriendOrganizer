2097c20e5f38707651c2f2c30051dbfa3900d18f
Артем Ищенко <rtemkaxxx@gmail.com>
2020-05-06 11:05:31 +0300

    FriendWrapper refactoring

--- FriendOrganizer.UI/Wrapper/FriendWrapper.cs
+++ FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -1,26 +1,17 @@
 ﻿using FriendOrganizer.Model;
-using FriendOrganizer.UI.ViewModel;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace FriendOrganizer.UI.Wrapper
 {
-    public class FriendWrapper : ViewModelBase, INotifyDataErrorInfo
+    public class FriendWrapper : NotifyDataInfoErrorBase
     {
-        private Dictionary<string, List<string>> errorsByProperty = new Dictionary<string, List<string>>();
-        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-
         public FriendWrapper(Friend friend)
         {
             Model = friend;
         }
 
-        public bool HasErrors => errorsByProperty.Any();
         public Friend Model { get; }
 
         public int Id
@@ -70,14 +61,7 @@ namespace FriendOrganizer.UI.Wrapper
                 OnPropertyChanged();
             }
         }
-
-        public IEnumerable GetErrors(string propertyName)
-        {
-            return errorsByProperty.ContainsKey(propertyName)
-                ? errorsByProperty[propertyName]
-                : null;
-        }
-
+        
         private void ValidateProperty([CallerMemberName]string propertyName = null)
         {
             ClearErrors(propertyName);
@@ -90,32 +74,6 @@ namespace FriendOrganizer.UI.Wrapper
                     }
                     break;
             }
-        }
-
-        private void AddError(string propertyName, string error)
-        {
-            if (!errorsByProperty.ContainsKey(propertyName))
-                errorsByProperty[propertyName] = new List<string>();
-
-            if (!errorsByProperty[propertyName].Contains(error))
-            {
-                errorsByProperty[propertyName].Add(error);
-                OnErrorsChanged(propertyName);
-            }
-        }
-
-        private void ClearErrors(string propertyName)
-        {
-            if (errorsByProperty.ContainsKey(propertyName))
-            {
-                errorsByProperty.Remove(propertyName);
-                OnErrorsChanged(propertyName);
-            }
-        }
-
-        private void OnErrorsChanged(string propertyName)
-        {
-            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
         }
     }
 }
---
+++ FriendOrganizer.UI/Wrapper/NotifyDataInfoErrorBase.cs
@@ -0,0 +1,50 @@
+﻿using FriendOrganizer.UI.ViewModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace FriendOrganizer.UI.Wrapper
+{
+    public class NotifyDataInfoErrorBase : ViewModelBase, INotifyDataErrorInfo
+    {
+        private Dictionary<string, List<string>> errorsByProperty = new Dictionary<string, List<string>>();
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        public bool HasErrors => errorsByProperty.Any();
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return errorsByProperty.ContainsKey(propertyName)
+                ? errorsByProperty[propertyName]
+                : null;
+        }
+
+        protected void AddError(string propertyName, string error)
+        {
+            if (!errorsByProperty.ContainsKey(propertyName))
+                errorsByProperty[propertyName] = new List<string>();
+
+            if (!errorsByProperty[propertyName].Contains(error))
+            {
+                errorsByProperty[propertyName].Add(error);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        protected void ClearErrors(string propertyName)
+        {
+            if (errorsByProperty.ContainsKey(propertyName))
+            {
+                errorsByProperty.Remove(propertyName);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        protected void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+    }
+
+}
